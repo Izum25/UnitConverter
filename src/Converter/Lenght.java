b3ba31c8915a5586caf7f10034c0a73d890eb0f9
UlduzSafarli <ulduzsafarli.dv@gmail.com>
2023-10-04 14:19:48 +0400

    Add Lenght class

---
+++ src/Converter/Lenght.java
@@ -0,0 +1,14 @@
+package Converter;
+
+import java.util.Scanner;
+
+public class Lenght {
+    private static void convertMetersToFeet(Scanner scanner) {
+        System.out.print("Введите длину в метрах: ");
+        double meters = scanner.nextDouble();
+
+        // 1 метр = 3.28084 фута
+        double feet = meters * 3.28084;
+        System.out.println("Результат: " + feet + " футов");
+    }
+}
